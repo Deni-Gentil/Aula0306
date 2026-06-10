fab5539a402e0be51ccda659e73ce7ed6cb4e93a
Deni-Gentil <deni.gentil@aluno.senai.br>
2026-06-10 17:40:06 -0300

    Add files via upload

---
+++ Lógica de Programação/Aula0306/primeiro_projeto.py
@@ -0,0 +1,12 @@
+# O símbolo de hashtag faz comentários em códigos python
+#==========================#
+#Unidade curricular:
+#Lógica de programação
+#Exercício 1:
+#Sistema de inicialização de segurança
+#==========================#
+print("---SISTEMA DE MANUFATURA AUTOMATIZADA---")
+print("Executando auto0teste de hardware")
+print("Sensores: OK")
+print("Atuadores: OK")
+print("Sistema pronto de operação")
